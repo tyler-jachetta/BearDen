41ab3dac78a4fd8f3b8c59e36475b20c43f0b28b
Tyler Jachetta <tjachetta@imgix.com>
2020-10-09 14:00:38 -0400

    Got a little more modular, and set up some homedir things and gcp

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -8,6 +8,7 @@ ROOTDIR="$(dirname $0)"
 sudo apt update
 sudo apt install -y software-properties-common
 sudo apt install -y openssh-server
+sudo apt install -y python3-pip
 
 sudo pip3 install ansible
 
