fba13dc3963f929e628cb63c6f40156b5d5cb7aa
Tyler Jachetta <tjachetta@imgix.com>
2020-10-09 09:25:04 -0400

    Initial commit of ansible stuff and things, should have been making more incremental commits

---
+++ bootstrap_ansible.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ROOTDIR="$(dirname $0)"
+
+# ToDo: bootstrap playbook
+
+# Install ansible
+sudo apt update
+sudo apt install -y software-properties-common
+sudo apt install -y openssh-server
+
+sudo pip3 install ansible
+
+# Create ssh keys and copy pubkey to authorized keys
+ssh-keygen -b 384 -t ecdsa -q -N "" -f ~/.ssh/id_ecdsa
+cat ~/.ssh/id_ecdsa.pub >> ~/.ssh/authorized_keys
+cat ~/.ssh/id_ecdsa.pub | sudo tee /root/.ssh/authorized_keys
+
+cd ansible
+ansible-playbook -K playbooks/den.yml
